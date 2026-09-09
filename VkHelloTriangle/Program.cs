f1dc129269af12ac981811cadd6d43c8c22a0bba
ousttrue <ousttrue@gmail.com>
2026-09-09 15:50:24 +0900

    remove app class. use using

--- VkHelloTriangle/Program.cs
+++ VkHelloTriangle/Program.cs
@@ -4,80 +4,55 @@ using System.Runtime.InteropServices;
 using Vortice.Vulkan;
 using static Vortice.Vulkan.Vulkan;
 
-unsafe class HelloTriangleApplication : IDisposable
+static class Program
 {
-    private readonly GlfwWindow _window;
-    private readonly InstanceObject _instance;
-    private readonly VkPhysicalDevice _physicalDevice;
-    private readonly DeviceObject _device;
-    private readonly SwapchainObject _swapchain;
-    private readonly PipelineObject _pipeline;
-
-    public HelloTriangleApplication()
+    public static unsafe void Main()
     {
-        _window = new GlfwWindow();
+        using var window = new GlfwWindow();
 
-        _instance = new InstanceObject(_window);
-        _physicalDevice = _instance.pickPhysicalDevice(DeviceObject.DeviceExtensions);
+        using var instance = new InstanceObject(window);
+        var physicalDevice = instance.pickPhysicalDevice(DeviceObject.DeviceExtensions);
 
         var deviceProperties = new VkPhysicalDeviceProperties2
         {
             sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2,
         };
-        _instance.Api.vkGetPhysicalDeviceProperties2(_physicalDevice, &deviceProperties);
+        instance.Api.vkGetPhysicalDeviceProperties2(physicalDevice, &deviceProperties);
         Console.Error.WriteLine(
             $"Selected device: {Marshal.PtrToStringAnsi((nint)deviceProperties.properties.deviceName) ?? throw new Exception()}"
         );
 
         var indices = QueueFamilyIndices.findQueueFamilies(
-            _instance.Api,
-            _physicalDevice,
-            _instance.Surface
+            instance.Api,
+            physicalDevice,
+            instance.Surface
         );
-        _device = new DeviceObject(
-            _instance.Api,
-            _physicalDevice,
+        using var device = new DeviceObject(
+            instance.Api,
+            physicalDevice,
             indices.GraphicsFamily,
             indices.PresentFamily
         );
-        _swapchain = new SwapchainObject(
-            _instance.Api,
-            _physicalDevice,
-            _instance.Surface,
-            _device.Api,
+        using var swapchain = new SwapchainObject(
+            instance.Api,
+            physicalDevice,
+            instance.Surface,
+            device.Api,
             new()
         );
-        _pipeline = new PipelineObject(_device.Api, _swapchain.RenderPass);
+        using var pipeline = new PipelineObject(device.Api, swapchain.RenderPass);
 
         while (true)
         {
-            if (!_window.NextFrame())
+            if (!window.NextFrame())
             {
                 break;
             }
-            var (imageIndex, commandBuffer) = _swapchain.Acquire();
-
-            _pipeline.RecordCommandBuffer(commandBuffer, _swapchain.Extent);
-
-            _swapchain.Present(imageIndex);
+            var (imageIndex, commandBuffer) = swapchain.Acquire();
+            pipeline.RecordCommandBuffer(commandBuffer, swapchain.Extent);
+            swapchain.Present(imageIndex);
         }
 
-        _device.Api.vkDeviceWaitIdle();
-    }
-
-    public void Dispose()
-    {
-        _swapchain.Dispose();
-        _device.Dispose();
-        _instance.Dispose();
-        _window.Dispose();
-    }
-}
-
-static class Program
-{
-    public static void Main()
-    {
-        using var app = new HelloTriangleApplication();
+        device.Api.vkDeviceWaitIdle();
     }
 }
